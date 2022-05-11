2fafdfa369802083321439cc398d1d0d1d66eac9
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-05-11 16:26:22 -0300

    FEAT: (frontend/developerMode) Adicionando requisito 8

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import StrangerThings from './components/StrangerThings';
 function App() {
   return (
     <div className="App">
-      <StrangerThings />
+      {
+        process.env.REACT_APP_HAWKINS_URL
+          ? <StrangerThings />
+          : <h1> Em desenvolvimento </h1>
+      }
     </div>
   );
 }
